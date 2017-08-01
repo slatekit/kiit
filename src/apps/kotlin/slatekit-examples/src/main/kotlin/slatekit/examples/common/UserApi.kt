978583ba453667974f3183124cffe3b7d712823f
kishore <kishore.pr4@gmail.com>
2017-08-01 10:26:16 -0400

    Examples: Updated

--- src/apps/kotlin/slatekit-examples/src/main/kotlin/slatekit/examples/common/UserApi.kt
+++ src/apps/kotlin/slatekit-examples/src/main/kotlin/slatekit/examples/common/UserApi.kt
@@ -19,11 +19,12 @@ import slatekit.common.DateTime
 import slatekit.common.Result
 import slatekit.common.results.ResultFuncs.ok
 import slatekit.core.common.AppContext
+import slatekit.integration.common.AppEntContext
 
 
 @Api(area = "app", name = "users", desc = "api to access and manage users 3",
      roles= "admin", auth = "app", verb = "*", protocol = "*")
-class UserApi(ctx:AppContext) : ApiBaseEntity<User>(ctx, User::class) {
+class UserApi(ctx: AppEntContext) : ApiBaseEntity<User, UserService>(ctx, User::class) {
 
   @ApiAction(name = "", desc = "activates a users account 3", roles= "@parent")
   fun activate(phone:String, code:Int, isPremiumUser:Boolean, date: DateTime): Result<Boolean> {
