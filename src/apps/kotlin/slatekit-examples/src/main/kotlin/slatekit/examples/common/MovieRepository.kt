3147fc5163c8ea74767e35e8efb0cbec31a5e3e0
kishore <kishore.pr4@gmail.com>
2018-02-10 15:55:44 -0500

    Examples: Deleted older directory in apps

--- src/apps/kotlin/slatekit-examples/src/main/kotlin/slatekit/examples/common/MovieRepository.kt
+++
@@ -1,8 +0,0 @@
-package slatekit.examples.common
-
-import slatekit.entities.repos.EntityRepoInMemory
-import kotlin.reflect.KClass
-
-
-class MovieRepository() : EntityRepoInMemory<Movie>(Movie::class) {
-}
