7808006ae33772d8c8de03e3d35252a82c4236b4
kishore <kishore.pr4@gmail.com>
2018-02-10 15:36:39 -0500

    CLI: Usage display and instructions

--- src/lib/kotlin/slatekit-core/src/main/kotlin/slatekit/core/cli/CliView.kt
+++ src/lib/kotlin/slatekit-core/src/main/kotlin/slatekit/core/cli/CliView.kt
@@ -18,8 +18,9 @@ import slatekit.common.console.ConsoleWriter
 
 
 class CliView(val _writer: ConsoleWriter,
-                val _infoCallback: ((Boolean, (Int, Pair<String, Any>) -> Unit) -> Unit)?
-) {
+              val _infoCallback: ((Boolean, (Int, Pair<String, Any>) -> Unit) -> Unit)?,
+              val _extendedInfo: ((ConsoleWriter) -> Unit)?
+                ) {
 
     /**
      * Shows general help info
@@ -28,45 +29,105 @@ class CliView(val _writer: ConsoleWriter,
         _writer.title("Please type your commands")
         _writer.line()
 
-        _writer.tab(1)
-        _writer.highlight("Syntax")
-        showHelpCommandSyntax()
+        showHelpInfo("1. Overview", listOf(
+                "This component exposes your applicable APIs on the CLI"
+        ))
 
-        _writer.tab(1)
-        _writer.highlight("Examples")
-        showHelpCommandExample()
+        showHelpInfo("2. Routing", listOf(
+                "Actions are in a 3 part hierarchical structure of {area}.{api}.{action}"
+        ))
 
-        _writer.tab(1)
-        _writer.highlight("Available")
+        showHelpInfo("3. Syntax", listOf(
+                "area.api.action  -param1=value* @param2='abc' @meta=value*"
+        ))
+
+        showHelpInfo("4. Examples", listOf(
+                "app.users.activate -email=johndoe@gmail.com @token=abc",
+                "To show examples for any action, type 'area.api.action ?' e.g. app.users.activate?"
+        ))
+
+        showHelpInfo("5. Parameters", listOf(
+                "There are 3 types of parameters that you can pass",
+                "1. regular   : begin with prefix '-' e.g. -name=john    : action parameters",
+                "2. meta      : begin with prefix '@' e.g. @token=abc    : action meta data",
+                "3. system    : begin with prefix '$' e.g. \$format=json  : command options"
+        ))
+
+        showHelpInfo("6. Batch", listOf(
+                "You can run commands in a batch, from a file in the inputs folder:",
+                "sys.cli.batch \$file='setup.txt'"
+        ))
+
+        showHelpInfo("7. Discovery", listOf(
+                "You can use a question mark '?' to display areas, apis, actions and action inputs",
+                "type ?                 : to list all areas",
+                "type area ?            : to list all apis in an area",
+                "type area.api ?        : to list all actions in an api",
+                "type area.api.action ? : to list all inputs for an action"
+        ))
+
+        _writer.highlight("8. Options")
+        showHelpCommandOptions()
+
+        _writer.highlight("9. Available")
         showHelpExtended()
 
         _writer.line()
         _writer.important("type 'exit' or 'quit' to quit program")
         _writer.url("type 'info' for detailed information")
-        _writer.success("type '?'                 : to list all areas")
-        _writer.success("type 'area ?'            : to list all apis in an area")
-        _writer.success("type 'area.api ?'        : to list all actions in an api")
-        _writer.success("type 'area.api.action ?' : to list all parameters for an action")
+    }
+
+
+    fun showHelpInfo(section:String, lines:List<String>):Unit {
+
+        _writer.highlight(section)
+        lines.forEach { line ->
+            //_writer.tab(1)
+            _writer.text(line)
+        }
         _writer.line()
     }
 
 
     /**
-     * Shows help command structure
+     * Shows help command for sys parameters
      */
-    fun showHelpCommandSyntax() {
-        _writer.tab(1)
-        _writer.text("area.api.action  -key=value*")
-        _writer.line()
-    }
+    fun showHelpCommandOptions() {
+        fun logSysOption(name:String, desc:String, required:Boolean, options:String):Unit {
+            val reqChar = if(required) "! required" else "? optional"
+            //_writer.tab(1)
+            _writer.text("\$$name  = $desc")
+            //_writer.tab(1)
+            _writer.text("           $reqChar $options")
 
+        }
+        // -env       = the environment to run in
+        //              ? optional  [String]  e.g. dev
+        _writer.text("These are the options available on every command")
+        logSysOption(
+                "file  ",
+                "loads inputs from the file path",
+                false,
+                "\"user://myapp/scripts/setup.txt\"")
 
-    /**
-     * Shows help command example syntax
-     */
-    fun showHelpCommandExample() {
-        _writer.tab(1)
-        _writer.text("app.users.activate -email=johndoe@gmail.com -role=user")
+        logSysOption(
+                "sample",
+                "generates a sample request file in the output directory",
+                false,
+                "\"sample1.json\"")
+
+        logSysOption(
+                "format",
+                "the format of the output",
+                false,
+                "[ json | csv | prop ]")
+
+        logSysOption(
+                "log   ",
+                "whether to log result to output directory",
+                false,
+                "true | false")
+
         _writer.line()
     }
 
@@ -75,6 +136,7 @@ class CliView(val _writer: ConsoleWriter,
      * Shows extra help - useful for derived classes to show more help info
      */
     fun showHelpExtended() {
+        _extendedInfo?.invoke(_writer)
     }
 
 
