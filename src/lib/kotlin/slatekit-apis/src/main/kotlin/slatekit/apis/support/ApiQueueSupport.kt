0b8d408373eddbb1369f773c389ea18f8287532e
kishore <kishore.pr4@gmail.com>
2018-05-28 01:22:56 -0400

    Moved reusable queue processing code to interface

--- src/lib/kotlin/slatekit-apis/src/main/kotlin/slatekit/apis/support/ApiQueueSupport.kt
+++ src/lib/kotlin/slatekit-apis/src/main/kotlin/slatekit/apis/support/ApiQueueSupport.kt
@@ -1,5 +1,12 @@
 package slatekit.apis.support
 
+import slatekit.apis.ApiConstants
+import slatekit.apis.core.Action
+import slatekit.apis.core.Requests
+import slatekit.common.Context
+import slatekit.common.Request
+import slatekit.common.ResultMsg
+import slatekit.common.Success
 import slatekit.common.queues.QueueSource
 
 interface ApiQueueSupport {
@@ -17,5 +24,22 @@ interface ApiQueueSupport {
         val pos = rand.nextInt(queues.size)
         val queue = queues[pos]
         queue.send(req)
+    }
+
+
+    /**
+     * Converts a request for an action that is queued, to an actual queue
+     */
+    fun sendToQueueOrProcess(ctx: Context, req: Request, target: Action, source: Any, args: Map<String, Any>?) : ResultMsg<String>  {
+        // Coming in as http request ? and mode is queued ?
+        return if(req.source != ApiConstants.SourceQueue && target.tag == "queued"){
+            // Convert from web request to Queued request
+            val queuedReq = Requests.convertToQueueRequest(req)
+            sendToQueue(queuedReq)
+            Success("Request processed as queue", Requests.codeHandlerProcessed)
+        }
+        else {
+            Success("Continue processing" , Requests.codeHandlerNotProcessed)
+        }
     }
 }
