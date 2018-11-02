d7c9c0a5d196b257e1b60d4e14286d485594395d
Kishore Reddy <kishore.reddy@weightwatchers.com>
2018-11-02 19:41:45 -0400

    Additional tests

---
+++ src/lib/kotlin/slatekit-tests/src/test/kotlin/test/entities/Entity_Mapper_ResultSet_Tests.kt
@@ -0,0 +1,143 @@
+/**
+<slate_header>
+url: www.slatekit.com
+git: www.github.com/code-helix/slatekit
+org: www.codehelix.co
+author: Kishore Reddy
+copyright: 2016 CodeHelix Solutions Inc.
+license: refer to website and/or github
+about: A Kotlin utility library, tool-kit and server backend.
+mantra: Simplicity above all else
+</slate_header>
+ */
+package test.entities
+
+import org.junit.Test
+import slatekit.common.records.RecordMap
+import slatekit.common.UniqueId
+import slatekit.common.utils.ListMap
+import slatekit.entities.core.EntityMapper
+import slatekit.entities.databases.vendors.MySqlConverter
+import slatekit.meta.models.ModelMapper
+import test.setup.*
+import java.sql.Timestamp
+import java.util.*
+
+/**
+ * Created by kishorereddy on 6/4/17.
+ */
+
+class Entity_Mapper_ResultSet_Tests {
+
+
+    @Test fun can_map_to_immutable_class(){
+        val model = ModelMapper.loadSchema(AuthorR::class, AuthorR::id.name)
+
+        val mapper = EntityMapper(model, MySqlConverter)
+        val data = buildSampleDataForAuthor()
+        val source = RecordMap(data)
+        val entity = mapper.mapFrom(source)!!as AuthorR
+
+        assert( entity.id == 1L )
+        assert( entity.uuid == "ABC" )
+        assert( entity.email == "kishore@abc.com" )
+        assert( entity.isActive )
+        assert( entity.age == 35 )
+        assert( entity.salary == 400.5 )
+        assert( entity.status == StatusEnum.Active )
+        assert( entity.uid == UUID.fromString(sampleUUID1) )
+        assert( entity.shardId == UniqueId.fromString(sampleUUID2) )
+    }
+
+
+    @Test fun can_map_to_writable_class(){
+        val model = ModelMapper.loadSchema(AuthorW::class, AuthorW::id.name)
+
+        val mapper = EntityMapper(model, MySqlConverter)
+        val data = buildSampleDataForAuthor()
+        val source = RecordMap(data)
+        val entity = mapper.mapFrom(source)!!as AuthorW
+
+        assert( entity.id == 1L )
+        assert( entity.uuid == "ABC" )
+        assert( entity.email == "kishore@abc.com" )
+        assert( entity.isActive )
+        assert( entity.age == 35 )
+        assert( entity.status == StatusEnum.Active )
+        assert( entity.salary == 400.5 )
+        assert( entity.uid == UUID.fromString(sampleUUID1) )
+        assert( entity.shardId == UniqueId.fromString(sampleUUID2) )
+    }
+
+
+    @Test fun can_map_to_immutable_class_with_embedded_object(){
+        val model = ModelMapper.loadSchema(UserWithAddress::class, UserWithAddress::id.name)
+
+        val mapper = EntityMapper(model, MySqlConverter)
+        val data = buildSampleDataForEmbeddedObject()
+        val source = RecordMap(data)
+        val entity = mapper.mapFrom(source)!!as UserWithAddress
+
+        assert( entity.id == 1L )
+        assert( entity.email == "kishore@abc.com" )
+        assert( entity.isActive )
+        assert( entity.age == 35 )
+        assert( entity.salary == 400.5 )
+        assert( entity.addr == Address("street 1", "city 1", "state 1", 1,"12345", true))
+        assert( entity.uid == UUID.fromString(sampleUUID1) )
+        assert( entity.shardId == UniqueId.fromString(sampleUUID2) )
+    }
+
+
+    fun buildSampleDataForAuthor(): ListMap<String, Any> {
+
+        val data = ListMap(
+            listOf(
+                Pair("id", 1L),
+                Pair("uuid", "ABC"),
+                Pair("createdAt", Timestamp(2017, 1, 1, 12, 0, 0, 0)),
+                Pair("createdBy", 100L),
+                Pair("updatedAt", Timestamp(2017, 1, 2, 12, 0, 0, 0)),
+                Pair("updatedBy", 101L),
+                Pair("email", "kishore@abc.com"),
+                Pair("isActive", true),
+                Pair("age", 35),
+                Pair("status", StatusEnum.Active.value),
+                Pair("salary", 400.5),
+                Pair("uid", UUID.fromString(sampleUUID1)),
+                Pair("shardId", UniqueId.fromString(sampleUUID2))
+            )
+        )
+        return data
+    }
+
+
+    fun buildSampleDataForEmbeddedObject(): ListMap<String, Any> {
+
+        val data = ListMap(
+            listOf(
+                Pair("id", 1L),
+                Pair("email", "kishore@abc.com"),
+                Pair("isActive", true),
+                Pair("age", 35),
+                Pair("salary", 400.5),
+                Pair("addr_addr", "street 1"),
+                Pair("addr_city", "city 1"),
+                Pair("addr_state", "state 1"),
+                Pair("addr_country", 1),
+                Pair("addr_zip", "12345"),
+                Pair("addr_isPOBox", true),
+                Pair("uid", UUID.fromString(sampleUUID1)),
+                Pair("shardId", UniqueId.fromString(sampleUUID2))
+            )
+        )
+        return data
+    }
+
+
+
+    companion object {
+        val sampleUUID1 = "67bdb72a-1d74-11e8-b467-0ed5f89f718b"
+        val sampleUUID2 = "67bdb72a-1d74-11e8-b467-0ed5f89f718c"
+    }
+}
