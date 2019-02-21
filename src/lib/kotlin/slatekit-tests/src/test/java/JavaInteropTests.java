10fc2823258707531d82d66c549f88b21e94828f
Kishore Reddy <kishore.reddy@weightwatchers.com>
2019-02-20 23:39:05 -0500

    FIX : Result usage

--- src/lib/kotlin/slatekit-tests/src/test/java/JavaInteropTests.java
+++ src/lib/kotlin/slatekit-tests/src/test/java/JavaInteropTests.java
@@ -2,13 +2,10 @@ import kotlin.Unit;
 import org.junit.Assert;
 import org.junit.Test;
 import slatekit.common.Random;
-import slatekit.common.Result;
-import slatekit.common.Success;
 import slatekit.common.envs.Env;
 import slatekit.common.envs.EnvMode;
 import slatekit.common.envs.Envs;
 import slatekit.common.info.Host;
-import slatekit.common.results.ResultCode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,22 +13,7 @@ import java.util.List;
 
 public class JavaInteropTests {
 
-    @Test
-    public void can_call_kotlin() {
-
-        Host host = new Host("mac1", "127.0.0.1", "", "osx", "10", "");
-        host.copy(host.name, host.ip, host.origin, host.arch, host.version, host.ext1);
-
-        Assert.assertTrue(ResultCode.BAD_REQUEST == 400);
-    }
-
-
-    @Test
-    public void can_use_result() {
-
-        Result<Integer, Exception> resS1 = new Success<Integer>(3, ResultCode.SUCCESS, "");
-        //Result<Integer, Exception> resS2 = success2(3, ResultCode.SUCCESS, "");
-    }
+    
 
 
     @Test
