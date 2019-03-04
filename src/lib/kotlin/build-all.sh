c3e8632f78ad4ac87cf0403adf923602d5437431
Kishore Reddy <kishore.reddy@weightwatchers.com>
2019-03-04 11:51:44 -0500

    Update build-all.sh

--- src/lib/kotlin/build-all.sh
+++ src/lib/kotlin/build-all.sh
@@ -1,21 +1,21 @@
 
-cd slatekit-common        && gradle clean build && cd ../ && sleep 5s
-cd slatekit-app           && gradle clean build && cd ../ && sleep 5s
+# cd slatekit-common        && gradle clean build && cd ../ && sleep 5s
+# cd slatekit-app           && gradle clean build && cd ../ && sleep 5s
 
-# cd slatekit-apis        && gradle clean build && cd ../       
-# cd slatekit-app         && gradle clean build && cd ../   
-# cd slatekit-cloud       && gradle clean build && cd ../     
-# cd slatekit-common      && gradle clean build && cd ../      
-# cd slatekit-core        && gradle clean build && cd ../    
-# cd slatekit-db          && gradle clean build && cd ../  
-# cd slatekit-entities    && gradle clean build && cd ../        
-# cd slatekit-examples    && gradle clean build && cd ../        
-# cd slatekit-integration && gradle clean build && cd ../           
-# cd slatekit-meta        && gradle clean build && cd ../    
-# cd slatekit-orm         && gradle clean build && cd ../   
-# cd slatekit-providers   && gradle clean build && cd ../         
-# cd slatekit-query       && gradle clean build && cd ../     
-# cd slatekit-server      && gradle clean build && cd ../      
-# cd slatekit-tests       && gradle clean build && cd ../     
-# cd slatekit-tools       && gradle clean build && cd ../     
-# cd slatekit-workers     && gradle clean build && cd ../       
+cd slatekit-common      && gradle clean build && cd ../ && echo 'sleeping 15s' && sleep 15s     
+cd slatekit-db          && gradle clean build && cd ../ && echo 'sleeping 15s' && sleep 15s 
+cd slatekit-query       && gradle clean build && cd ../ && echo 'sleeping 15s' && sleep 15s    
+cd slatekit-meta        && gradle clean build && cd ../ && echo 'sleeping 15s' && sleep 15s   
+cd slatekit-app         && gradle clean build && cd ../ && echo 'sleeping 15s' && sleep 15s  
+cd slatekit-workers     && gradle clean build && cd ../ && echo 'sleeping 15s' && sleep 15s      
+cd slatekit-core        && gradle clean build && cd ../ && echo 'sleeping 15s' && sleep 15s   
+cd slatekit-apis        && gradle clean build && cd ../ && echo 'sleeping 15s' && sleep 15s      
+cd slatekit-cloud       && gradle clean build && cd ../ && echo 'sleeping 15s' && sleep 15s    
+cd slatekit-entities    && gradle clean build && cd ../ && echo 'sleeping 15s' && sleep 15s       
+cd slatekit-orm         && gradle clean build && cd ../ && echo 'sleeping 15s' && sleep 15s  
+cd slatekit-integration && gradle clean build && cd ../ && echo 'sleeping 15s' && sleep 15s          
+cd slatekit-providers   && gradle clean build && cd ../ && echo 'sleeping 15s' && sleep 15s        
+cd slatekit-server      && gradle clean build && cd ../ && echo 'sleeping 15s' && sleep 15s   
+#cd slatekit-tools       && gradle clean build && cd ../ && echo 'sleeping 5s' && sleep 5s       
+#cd slatekit-examples    && gradle clean build && cd ../ && echo 'sleeping 5s' && sleep 5s     
+#cd slatekit-tests       && gradle clean build && cd ../ && sleep 5s 
