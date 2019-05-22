b1cd60e0689016fccee865c53511b2ec14b15f85
Kishore Reddy <kishore.reddy@weightwatchers.com>
2019-05-22 15:41:22 -0400

    Update EntityDynamic.kt

--- src/lib/kotlin/slatekit-entities/src/main/kotlin/slatekit/entities/features/EntityDynamic.kt
+++ src/lib/kotlin/slatekit-entities/src/main/kotlin/slatekit/entities/features/EntityDynamic.kt
@@ -5,7 +5,15 @@ import slatekit.results.Try
 
 /**
  * Dynamic CRUD Support without knowning what type of Entity[T] it is.
- * This just requires knowing the Id type
+ * This just requires knowing the Id type.
+ *
+ * - entities: mode as enum/sealed trait
+ * - entities: generic interface
+ * - entities: convenience method for uuid ?
+ * - entities: in-memory repo with model
+ * - entities: in-memory repo with protected items
+ * - entities: in-memory repo find fix
+ * - entities: crud with result ?
  */
 interface EntityDynamic<TId> where TId:Comparable<TId> {
 
