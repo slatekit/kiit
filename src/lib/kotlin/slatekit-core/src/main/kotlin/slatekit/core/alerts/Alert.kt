e6d85ccb772e7943b0cf58e6a5d8df856f125b66
Kishore Reddy <kishore.reddy@weightwatchers.com>
2019-09-21 19:35:25 -0400

    FIX: Codes for alert and fields

--- src/lib/kotlin/slatekit-core/src/main/kotlin/slatekit/core/alerts/Alert.kt
+++ src/lib/kotlin/slatekit-core/src/main/kotlin/slatekit/core/alerts/Alert.kt
@@ -1,23 +1,24 @@
 package slatekit.core.alerts
 
 /**
- * @param area   : logical area of an application
+ * @param area   : logical group/project this alert is associated with
+ * @param agent  : application / service sending this alert
  * @param name   : name of the alert        e.g. "NEW_DEVICE_REGISTRATION"
+ * @param env    : environment of this alert e.g. dev | qat | pro
  * @param uuid   : unique id ( UUID ) identification
  * @param desc   : description of the alert e.g. "User registration via mobile"
- * @param success: whether or not this is a success or failure
- * @param status : integer code for more specific status
+ * @param code   : describes the intent of the alert ( success / failure, progress, etc )
  * @param target : name / id of the target/destination, should match the @see [AlertTarget.target]
  * @param tag    : correlation id/tag for linking to other items
  * @param fields : Optional list of additional fields related to the alert
  *
  * event : {
- *     area: 'app.registration.new',
- *     name: 'NEW_DEVICE_REGISTRATION',
+ *     area: 'registration',
+ *     agent:'reg-service-prod',
+ *     name: 'NEW_ANDROID_REGISTRATION',
  *     uuid: 'abc-123-xyz',
  *     desc: 'User registration via mobile',
- *     success: true,
- *     code: 200,
+ *     code: AlertCodes.SUCCESS,
  *     target: "registration-alerts",
  *     tag : "a1b2c3",
  *     fields: [
@@ -28,11 +29,12 @@ package slatekit.core.alerts
  */
 data class Alert(
                  val area: String,
+                 val agent: String,
                  val name: String,
+                 val env : String,
                  val uuid: String,
                  val desc: String,
-                 val success: Boolean,
-                 val status: Int,
+                 val code: AlertCode,
                  val target: String,
                  val tag: String,
                  val fields: List<AlertField>?
