1fb1b348ea2be5e59b30379ccf528eb1e38cec3c
Kishore Reddy <kishore.reddy@weightwatchers.com>
2019-10-14 20:58:46 -0400

    ADD: Missing dependencies

--- src/lib/kotlin/slatekit-integration/src/main/kotlin/slatekit/integration/apis/SmsApi.kt
+++ src/lib/kotlin/slatekit-integration/src/main/kotlin/slatekit/integration/apis/SmsApi.kt
@@ -21,7 +21,7 @@ import slatekit.apis.security.Verbs
 import slatekit.apis.support.ApiWithSupport
 import slatekit.common.Context
 import slatekit.common.Vars
-import slatekit.core.sms.SmsService
+import slatekit.notifications.sms.SmsService
 import slatekit.results.Outcome
 import slatekit.results.Try
 
