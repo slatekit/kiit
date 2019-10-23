73e9d4a027feb34b4c7871f190daa82ef893bd42
Kishore Reddy <kishore.reddy@weightwatchers.com>
2019-10-22 21:05:54 -0400

    DELETE: Workers module ( replaced by Jobs )

--- src/lib/kotlin/slatekit-workers/src/main/kotlin/slatekit/workers/Event.kt
+++
@@ -1,8 +0,0 @@
-package slatekit.workers
-
-sealed class Event {
-    data class JobEvent    (val sender:String, val name:String, val state:String, val job:Job) : Event()
-    data class QueueEvent  (val sender:String, val name:String, val state:String, val queue: Queue?) : Event()
-    data class WorkerEvent (val sender:String, val name:String, val state:String, val worker:Worker<*>?) : Event()
-    data class ManagerEvent(val sender:String, val name:String, val state:String, val manager:Manager?) : Event()
-}
--- src/lib/kotlin/slatekit-workers/src/main/kotlin/slatekit/workers/WorkRequest.kt
+++
@@ -1,3 +0,0 @@
-package slatekit.workers
-
-data class WorkRequest(val job:Job, val queue: Queue, val worker:Worker<*>)
