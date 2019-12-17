a513b7639470d97716f7089178a71c27bb58ba48
Kishore Reddy <kishore.reddy@weightwatchers.com>
2019-12-17 18:45:49 -0500

    Build 0.9.37

--- src/lib/kotlin/build-all.sh
+++ src/lib/kotlin/build-all.sh
@@ -1,34 +1,35 @@
 
 # Foundational
-cd slatekit-result        && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s     
-cd slatekit-common        && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s     
-cd slatekit-tracking      && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s   
-cd slatekit-functions     && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s   
-cd slatekit-meta          && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s   
-cd slatekit-core          && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s   
+cd slatekit-result        && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s     
+cd slatekit-common        && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s     
+cd slatekit-tracking      && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s   
+cd slatekit-functions     && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s   
+cd slatekit-meta          && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s   
+cd slatekit-core          && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s   
 
 # Apps / Services
-cd slatekit-app           && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s  
-cd slatekit-cli           && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s  
-cd slatekit-jobs          && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s 
-cd slatekit-apis          && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s 
-cd slatekit-cache         && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s 
-cd slatekit-cmds          && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s 
+cd slatekit-app           && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s  
+cd slatekit-cli           && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s  
+cd slatekit-jobs          && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s 
+cd slatekit-apis          && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s 
+cd slatekit-cache         && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s 
+cd slatekit-cmds          && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s 
 
 # Infrastructure
-cd slatekit-db            && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s 
-cd slatekit-cloud         && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s    
-cd slatekit-notifications && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s   
+cd slatekit-db            && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s 
+cd slatekit-cloud         && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s    
+cd slatekit-notifications && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s   
 
 # Entities / ORM
-cd slatekit-query         && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s    
-cd slatekit-entities      && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s       
-cd slatekit-orm           && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s  
+cd slatekit-query         && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s    
+cd slatekit-entities      && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s       
+cd slatekit-orm           && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s  
 
 # Integration
-cd slatekit-integration   && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s          
-cd slatekit-providers     && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s        
-cd slatekit-server        && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 15s' && sleep 15s   
+cd slatekit-integration   && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s          
+cd slatekit-providers     && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s        
+cd slatekit-server        && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s   
+cd slatekit-generator     && gradle clean build bintrayUpload && cd ../ && echo 'sleeping 12s' && sleep 12s   
 #cd slatekit-tools       && gradle clean build && cd ../ && echo 'sleeping 5s' && sleep 5s       
 #cd slatekit-examples    && gradle clean build && cd ../ && echo 'sleeping 5s' && sleep 5s     
 #cd slatekit-tests       && gradle clean build && cd ../ && sleep 5s 
