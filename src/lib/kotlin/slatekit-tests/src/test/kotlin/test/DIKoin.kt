a5735118577671eba61bab45f34f9b4c49f836e0
Kishore Reddy <kishore.reddy@weightwatchers.com>
2020-01-17 22:23:24 -0500

    Update: Templates

--- src/lib/kotlin/slatekit-tests/src/test/kotlin/test/DIKoin.kt
+++ src/lib/kotlin/slatekit-tests/src/test/kotlin/test/DIKoin.kt
@@ -7,7 +7,7 @@ package test
 //import org.koin.core.module.Module
 //import org.koin.dsl.module
 //import slatekit.common.utils.Random
-//import slatekit.common.encrypt.HMAC
+//import slatekit.common.crypto.HMAC
 //import slatekit.common.info.ApiLogin
 //import slatekit.core.email.EmailService
 //import slatekit.core.email.EmailServiceSendGrid
