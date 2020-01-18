fd3ff885e7f695bcb5b335fe9738317143995964
Kishore Reddy <kishore.reddy@weightwatchers.com>
2020-01-17 21:39:30 -0500

    Update Example_Cloud_Queues.kt

--- src/lib/kotlin/slatekit-examples/src/main/kotlin/slatekit/examples/Example_Cloud_Queues.kt
+++ src/lib/kotlin/slatekit-examples/src/main/kotlin/slatekit/examples/Example_Cloud_Queues.kt
@@ -15,7 +15,10 @@ package slatekit.examples
 
 //<doc:import_required>
 import slatekit.cloud.aws.SQS
+import slatekit.core.queues.QueueEntry
 import slatekit.core.queues.QueueStringConverter
+import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.amazonaws.regions.Regions
 
 //</doc:import_required>
 
@@ -24,8 +27,6 @@ import slatekit.cmds.Command
 import slatekit.cmds.CommandRequest
 import slatekit.results.Success
 import slatekit.results.Try
-import com.amazonaws.auth.profile.ProfileCredentialsProvider
-import com.amazonaws.regions.Regions
 import kotlinx.coroutines.runBlocking
 import slatekit.results.getOrElse
 
@@ -42,19 +43,7 @@ class Example_Cloud_Queues : Command("sqs") {
         // 1. This can be String converter for any payload ( e.g. JSON )
         // 2. You can create a custom type for type safety
         val converter = QueueStringConverter()
-        /**
-         *  PATHS:
-         *  1. /.aws/credentials
-         *  2. ~/.slatekit/conf/files.conf
-         *
-         *  CONTENT:
-         *  queues = true
-         *  queues.account =
-         *  queues.key  = AWS_KEY_HERE
-         *  queues.pass = AWS_PASSWORD_HERE
-         *  queues.env  = dev
-         *  queues.tag  = samples
-         */
+
         // Not storing any key/secret in source code for security purposes
         // Setup 1: Use the default aws config file in "~/.aws/credentials and supply AWS region
         val queue1 = SQS<String>(credentials = ProfileCredentialsProvider().credentials,
@@ -65,6 +54,14 @@ class Example_Cloud_Queues : Command("sqs") {
 
         // Setup 3: Use the config at "~/myapp/conf/queue.conf"
         // Reads from the section "queues" by default
+        /**
+         *  SAMPLE CONFIG:
+         *  queues = true
+         *  queues.key  = AWS_KEY_HERE
+         *  queues.pass = AWS_PASSWORD_HERE
+         *  queues.env  = dev
+         *  queues.tag  = samples
+         */
         val queue3 = SQS.of<String>( region = "us-east-1", name = "slatekit", converter = converter,
                 confPath = "~/myapp/conf/queue.conf", confSection = "queues")
 
@@ -77,8 +74,11 @@ class Example_Cloud_Queues : Command("sqs") {
             // Use case 1: init()
             queue.init()
 
-            // Use case 2: send 1 message
-            queue.send("item 1")
+            // NOTES:
+            // 1. All operations use the slate kit Result<T,E> type
+            // 2. All operations return a slate kit Try<T> = Result<T, Exception>
+            // Use case 2: send 1 message ( get back message id as String )
+            val result1:Try<String> = queue.send("item 1")
 
             // Use case 3: send multiple messages
             queue.send("item 2")
