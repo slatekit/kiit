0e46f7288856f3459972a9bac60cee5addbcd69a
Kishore Reddy <kishore.reddy@weightwatchers.com>
2020-02-07 17:00:13 -0500

    Update Cache_Channel_Tests.kt

--- src/lib/kotlin/slatekit-tests/src/test/kotlin/test/cache/Cache_Channel_Tests.kt
+++ src/lib/kotlin/slatekit-tests/src/test/kotlin/test/cache/Cache_Channel_Tests.kt
@@ -25,8 +25,9 @@ import test.cache.MockCacheCoordinator
 class Cache_Channel_Tests {
 
     fun getCache(initialize:Boolean = true, settings: CacheSettings = CacheSettings(10), listener:((CacheEvent) -> Unit)? = null): SimpleAsyncCache {
-        val raw =  SimpleCache("async-cache", settings = settings, listener = listener)
-        val cache = SimpleAsyncCache(raw, MockCacheCoordinator(LoggerConsole(), Paired()))
+        val logger = LoggerConsole()
+        val raw =  SimpleCache("async-cache", settings = settings, listener = listener, logger = logger)
+        val cache = SimpleAsyncCache(raw, MockCacheCoordinator(logger, Paired()))
         if(initialize) {
             cache.put("countries", "countries supported for mobile app", 60) { listOf("us", "ca") }
             runBlocking {
