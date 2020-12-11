ad3045023fc34cdc20664b8fc52b2fcfa3f42763
kishore <kishore.pr4@gmail.com>
2020-12-10 21:08:51 -0500

    Remove: Worker components from actor.

--- src/lib/kotlin/slatekit-actors/src/main/kotlin/slatekit/actors/WContext.kt
+++
@@ -1,5 +0,0 @@
-package slatekit.actors
-
-import java.util.concurrent.atomic.AtomicReference
-
-data class WContext<T>(val worker: Worker<T>, val status: AtomicReference<Status> = AtomicReference(Status.InActive))
--- src/lib/kotlin/slatekit-actors/src/main/kotlin/slatekit/actors/Worker.kt
+++
@@ -1,6 +0,0 @@
-package slatekit.actors
-
-
-abstract class Worker<T>(val id: String) : Cycle {
-    abstract suspend fun work(item: T): Result
-}
