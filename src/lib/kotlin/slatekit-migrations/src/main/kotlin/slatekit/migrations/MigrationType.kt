92a5ba1ee226a05ea575c57ebf346dd482f3d8f8
kishore <kishore.pr4@gmail.com>
2021-01-22 21:51:06 -0500

    ADD: slatekit.migrations

---
+++ src/lib/kotlin/slatekit-migrations/src/main/kotlin/slatekit/migrations/MigrationType.kt
@@ -0,0 +1,8 @@
+package slatekit.migrations
+
+sealed class MigrationType {
+    object TableCreate : MigrationType()
+    object TableUpdate : MigrationType()
+    object TableDelete : MigrationType()
+    object Custom : MigrationType()
+}
