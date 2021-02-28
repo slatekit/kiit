3ebe0ded11ee7286b0afc95960511fb134734ff2
kishore <kishore.pr4@gmail.com>
2021-02-28 17:51:03 -0500

    FIX: Sqlite issues

--- src/lib/kotlin/slatekit-entities/src/main/kotlin/slatekit/entities/EntityRepo.kt
+++ src/lib/kotlin/slatekit-entities/src/main/kotlin/slatekit/entities/EntityRepo.kt
@@ -13,6 +13,8 @@
 
 package slatekit.entities
 
+import slatekit.common.data.BuildMode
+import slatekit.common.data.DataHooks
 import slatekit.common.data.IDb
 import slatekit.data.Mapper
 import slatekit.data.SqlRepo
@@ -25,10 +27,14 @@ import slatekit.entities.mapper.EntityMapper
  * @tparam T
  */
 open class EntityRepo<TId, T>(
-        db: IDb,
-        meta: Meta<TId, T>,
-        mapper: Mapper<TId, T>,
-        syntax: Provider<TId, T>) : SqlRepo<TId, T>(db, meta, mapper, syntax) where TId : Comparable<TId>, T : Any {
+    db: IDb,
+    meta: Meta<TId, T>,
+    mapper: Mapper<TId, T>,
+    syntax: Provider<TId, T>,
+    hooks: DataHooks<TId, T>? = null,
+    mode: BuildMode = BuildMode.Prep)
+    : SqlRepo<TId, T>(db, meta, mapper, syntax, hooks, mode) where TId : Comparable<TId>, T : Any {
+
     private val lookup:Map<String, String> = if(mapper is EntityMapper<*, *>) {
         mapper.model.fields.map { it.name to it.storedName }.toMap()
     }
