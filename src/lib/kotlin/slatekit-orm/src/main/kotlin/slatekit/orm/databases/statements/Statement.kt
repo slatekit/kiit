a32921b2fe5f67d3a759fb06ce574d5e696d129b
kishore <kishore.pr4@gmail.com>
2021-11-18 23:14:46 -0500

    CLEANUP: Delete Orm/Tools, Move examples to apps

--- src/lib/kotlin/slatekit-orm/src/main/kotlin/slatekit/orm/databases/statements/Statement.kt
+++
@@ -1,9 +0,0 @@
-package slatekit.orm.databases.statements
-
-import slatekit.orm.OrmMapper
-import slatekit.meta.models.Model
-
-interface Statement<TId, T> where TId: kotlin.Comparable<TId>, T: Any {
-
-    fun sql(item: T, model: Model, mapper: OrmMapper<TId, T>):String
-}
