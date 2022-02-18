d9cd57c4bc5d223b3c1e2d3c4669d69d27b6e2ae
kishore <kishore.pr4@gmail.com>
2022-02-17 21:16:49 -0500

    WIP

--- src/lib/kotlin/slatekit-status/src/commonMain/kotlin/slatekit/status/Sample.kt
+++ src/lib/kotlin/slatekit-status/src/commonMain/kotlin/slatekit/status/Sample.kt
@@ -9,3 +9,13 @@ expect object Platform {
 }
 
 fun hello(): String = "Hello from ${Platform.name}"
+
+/*
+1. create
+2. compile
+3. test
+4. gradle compile, test
+5. repository setup
+6. publishing
+7. reuse
+ */
