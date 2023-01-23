45ca0957e3c03913faba824ad8d8bd9488190d83
kishore <kishore.pr4@gmail.com>
2023-01-22 21:25:47 -0500

    BUILD: Fix missing files (#351)
    * BUILD: update settings ( part of tests integration )
    
    * BUILD: git actions to include tests on branches
    
    * BUILD: sample commands for release

--- src/build-tag.sh
+++ src/build-tag.sh
@@ -10,6 +10,10 @@ FULL_TAG=srv_${MAJOR}_${MINOR}_${PATCH}_${BUILD}_${TAG}
 echo ${BRANCH}
 echo ${VERSION}
 
-$(git tag ${FULL_TAG})
+# git tag -a v2.10.1 -m "New release"
+# git release v2.10.1 -m "Kiit Release 2.10.1"
+# gh release create v2.10.1 --notes "Kiit Release 2.10.1"
+# git push --tags
+$(git tag -a ${TAG_NAME} -m "New release")
 $(git push --tags)
 exit 0
