79e265ec2434e1a703c87b14f74f7c7fce86a133
kishore <kishore.pr4@gmail.com>
2023-01-28 19:01:58 -0500

    tests: logging for ci checks

--- src/lib/kotlin/slatekit-tests/src/test/kotlin/test/entities/Data_03_Statement_Syntax.kt
+++ src/lib/kotlin/slatekit-tests/src/test/kotlin/test/entities/Data_03_Statement_Syntax.kt
@@ -22,13 +22,25 @@ class Data_03_Statement_Syntax {
 
     @Test
     fun can_build_insert() {
-        val model = Schema.load(SampleEntityImmutable::class)
-        val mapper = EntityMapper<Long, SampleEntityImmutable>(model, EntitySetup.meta, Long::class, SampleEntityImmutable::class, EntitySettings(false))
-        val stmt = Insert<Long, SampleEntityImmutable>(MySqlDialect, EntitySetup.meta, mapper)
-        val sample = EntitySetup.sampleImmutable()
-        val actual = stmt.build(sample, BuildMode.Sql).sql
-        val expected = """insert into `sample1` (`test_string`,`test_string_enc`,`test_bool`,`test_short`,`test_int`,`test_long`,`test_float`,`test_double`,`test_enum`,`test_localdate`,`test_localtime`,`test_localdatetime`,`test_zoneddatetime`,`test_uuid`,`test_uniqueId`,`test_object_addr`,`test_object_city`,`test_object_state`,`test_object_country`,`test_object_zip`,`test_object_isPOBox`)  VALUES ('abc','abc123',0,1,2,3,4.5,5.5,1,'2021-01-20','13:30:45','2021-01-20 13:30:45','2021-01-20 13:30:45','497dea41-8658-4bb7-902c-361014799214','usa:314fef51-43a7-496c-be24-520e73758836','street 1','city 1','state 1',1,'12345',1);"""
-        Assert.assertEquals(expected, actual)
+        try {
+            val model = Schema.load(SampleEntityImmutable::class)
+            val mapper = EntityMapper<Long, SampleEntityImmutable>(
+                model,
+                EntitySetup.meta,
+                Long::class,
+                SampleEntityImmutable::class,
+                EntitySettings(false)
+            )
+            val stmt = Insert<Long, SampleEntityImmutable>(MySqlDialect, EntitySetup.meta, mapper)
+            val sample = EntitySetup.sampleImmutable()
+            val actual = stmt.build(sample, BuildMode.Sql).sql
+            val expected =
+                """insert into `sample1` (`test_string`,`test_string_enc`,`test_bool`,`test_short`,`test_int`,`test_long`,`test_float`,`test_double`,`test_enum`,`test_localdate`,`test_localtime`,`test_localdatetime`,`test_zoneddatetime`,`test_uuid`,`test_uniqueId`,`test_object_addr`,`test_object_city`,`test_object_state`,`test_object_country`,`test_object_zip`,`test_object_isPOBox`)  VALUES ('abc','abc123',0,1,2,3,4.5,5.5,1,'2021-01-20','13:30:45','2021-01-20 13:30:45','2021-01-20 13:30:45','497dea41-8658-4bb7-902c-361014799214','usa:314fef51-43a7-496c-be24-520e73758836','street 1','city 1','state 1',1,'12345',1);"""
+            Assert.assertEquals(expected, actual)
+        }catch(ex:Exception){
+            println(ex.message)
+            throw ex 
+        }
     }
 
 
