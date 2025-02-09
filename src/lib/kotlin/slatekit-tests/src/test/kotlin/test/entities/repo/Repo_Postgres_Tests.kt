4c70eb2319d9b3c9259c863aa430851b14568d59
kishore <kishore.pr4@gmail.com>
2025-02-09 14:44:12 -0500

    tests: add repo tests for postgres

---
+++ src/lib/kotlin/slatekit-tests/src/test/kotlin/test/entities/repo/Repo_Postgres_Tests.kt
@@ -0,0 +1,31 @@
+package test.entities.repo
+
+import kiit.common.data.Vendor
+import kiit.entities.Entities
+import kiit.entities.EntityLongId
+import kiit.entities.EntityRepo
+import kotlinx.coroutines.runBlocking
+import test.entities.EntitySetup
+import test.setup.User5
+
+/**
+Sql scripts located :
+1. ./install/db/mysql/3.x/
+2. ./install/db/postgres/3.x/
+ */
+
+
+open class Repo_Postgres_Tests : Repo_Common_Tests() {
+
+    /**
+     * This provices the implementation for the repo ( using mysql as vendor )
+     */
+    override fun process(op: (EntityRepo<Long, User5>) -> Unit) {
+        val db = EntitySetup.db(Vendor.Postgres)
+        val entities = Entities( {_ -> db } )
+        val repo = entities.repo(EntityLongId(), Long::class, User5::class, "user", null, Vendor.Postgres)
+        runBlocking {
+            op(repo)
+        }
+    }
+}
