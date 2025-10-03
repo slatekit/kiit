ebf6b2b544ecdd48f3361988dc1089b61c8c51ab
kishore <kishore.pr4@gmail.com>
2025-10-02 20:06:47 -0400

    fix: compile issues on server module (#384)

--- src/lib/kotlin/slatekit-tests/src/test/kotlin/test/apis/Api_001_Router_Tests.kt
+++ src/lib/kotlin/slatekit-tests/src/test/kotlin/test/apis/Api_001_Router_Tests.kt
@@ -3,6 +3,7 @@ package test.apis
 import kiit.apis.setup.api
 import kiit.apis.setup.router
 import kiit.context.AppContext
+import org.junit.Assert
 import org.junit.Test
 import test.setup.SampleApi1V1
 import test.setup.SampleApi2V2
@@ -18,6 +19,16 @@ class Api_001_Router_Tests : ApiTestsBase() {
                 api(SampleApi2V2::class, SampleApi2V2(context))
             )
         )
+
+        val allRoutes = router.routes.items.map { it.items.map { it } }
+        // This is for backwards compatibility /{area}/{api}/{action} ( without version )
+        val allVersions = allRoutes.map { actions ->
+            actions.map { action -> action.api.version }
+        }.flatten()
+        println(allVersions)
+
+        Assert.assertEquals("v1", allVersions[0])
+        Assert.assertEquals("v2", allVersions[1])
 
         router.routes.items.forEachIndexed { ndx, area ->
             println("Version ndx = $ndx, name = ${area.name}")
